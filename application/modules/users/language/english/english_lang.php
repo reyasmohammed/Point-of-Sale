ff8d14804c36df25fd5f17af639c5826a3279694
jibigopi <jibigopi007@gmail.com>
2014-05-22 23:20:02 +0530

    language files deleted

--- application/modules/users/language/english/english_lang.php
+++
@@ -1,6 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-$lang['login']='Login';
-$lang['jibigopi99999999999999999999999999999999999999999999999999999999']='jibiyuiyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyygopi';
-
-  
-?>
