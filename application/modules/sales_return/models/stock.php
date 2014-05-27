5bdf78136dddcf6de28216f2135fd3dc206c745a
jibigopi <jibigopi007@gmail.com>
2014-05-27 23:40:48 +0530

    sales return

--- application/modules/sales_return/models/stock.php
+++ application/modules/sales_return/models/stock.php
@@ -37,14 +37,13 @@ class Stock extends CI_Model{
          $this->db->update('sales_return_x_items',array('guid'=>md5('sales_return_x_items'.$item.$os_item)));
     }
     
-    function search_items($search){
+    function search_items($search,$bill){
         $bid=$this->session->userdata['branch_id'];
        
-        $this->db->select('items.uom,items.no_of_unit,stocks_history.guid as stock_id,suppliers.first_name,suppliers.guid as s_guid,items_setting.sales_return,items.tax_Inclusive ,tax_types.type as tax_type_name,taxes.value as tax_value,taxes.type as tax_type,brands.name as b_name,items_department.department_name as d_name,items_category.category_name as c_name,items.name,items.guid as i_guid,items.code,items.image,items.tax_Inclusive,items.tax_id,stocks_history.price as price,stocks_history.cost as cost,stock.quty as quty')->from('stock')->where('stock.branch_id',$bid)->where('stock.quty >',0);
+        $this->db->select('items.uom,items.no_of_unit,suppliers.first_name,suppliers.guid as s_guid,items_setting.sales_return,items.tax_Inclusive ,tax_types.type as tax_type_name,taxes.value as tax_value,taxes.type as tax_type,brands.name as b_name,items_department.department_name as d_name,items_category.category_name as c_name,items.name,items.guid as i_guid,items.code,items.image,items.tax_Inclusive,items.tax_id,')->from('sales_bill')->where('sales_bill.guid',$bill);
         //$this->db->distinct('stocks_history.cost');
-        $this->db->join('stocks_history', 'stocks_history.stock_id=stock.guid','left');
-        $this->db->join('suppliers', 'suppliers.guid=stocks_history.supplier_id','left');
-        $this->db->join('items', 'items.guid=stocks_history.item_id','left');
+        $this->db->join('delivery_note_x_items', 'delivery_note_x_items.delivery_note=sales_bill.sdn','left');
+        $this->db->join('items', 'items.guid=delivery_note_x_items.item','left');
         $this->db->join('items_category', 'items.category_id=items_category.guid','left');
         $this->db->join('brands', 'items.brand_id=brands.guid','left');
         $this->db->join('items_setting', 'items.guid=items_setting.item_id AND items_setting.purchase=1','left');
@@ -135,15 +134,15 @@ class Stock extends CI_Model{
      }
       function search_sales_bill($search){
           
-          $this->db->select('sales_bill.*')->from('sales_bill');
+          $this->db->select('sales_bill.*,customers.first_name,customers.company_name')->from('sales_bill');
           $this->db->join('customers', 'customers.guid=sales_bill.customer_id','left');
-          $like=array('invoice'=>$search,'code'=>$search,'phone'=>$search,'email'=>$search,'city'=>$search,'state'=>$search,'country'=>$search,'website'=>$search);       
+          $like=array('invoice'=>$search,'first_name'=>$search,'company_name'=>$search);       
           $this->db->or_like($like);
           $this->db->limit($this->session->userdata('data_limit'));
           $sql=  $this->db->get();
           $data=array();
-          foreach($sql->result() as $row){
-            
+          foreach($sql->result_array() as $row){
+           $row['date']=date('d-m-Y',$row['date']);
                   $data[]=$row;
           }
           return $data;
